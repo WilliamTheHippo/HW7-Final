81eeec484d795573d6cc629d8f18e12f09792367
Alex Gould <ag4536@nyu.edu>
2020-11-23 13:39:31 -0500

    refactor

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,121 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public float moveSpeed;
-	//public bool onDoorTrigger;
-
 	[Header("Sprites")]
 	public Sprite front;
 	public Sprite back;
 	public Sprite side;
-	public Sprite[] walk;
-	int walk_offset;
-
-	CameraMovement cam;
-	enum Direction {Up, Down, Left, Right}
-	Direction direction;
-	SpriteRenderer sr;
 	Sprite idle;
 
-	bool walking;
+	public enum Direction {Up, Down, Left, Right}
+
+	[Header("State Management")]
+	public Direction direction;
+	public bool walking;
+
+	SpriteRenderer sr;
+	CameraMovement cam;
+	Walking walk;
 
 	void Start()
 	{
 		sr = GetComponent<SpriteRenderer>();
-		idle = sr.sprite;
 		cam = Camera.main.GetComponent<CameraMovement>();
+		idle = sr.sprite;
 		direction = Direction.Down;
+
+		walk = GetComponent<Walking>();
 		walking = false;
 	}
 
 	void FixedUpdate()
 	{
 		if(cam.Panning) return;
-
-		//WALKING
-		float old_x = transform.position.x;
-		float old_y = transform.position.y;
-		bool keyDown = false;
-		walk_offset = 0;
-		if(direction == Direction.Down) walk_offset += 2;
-		if(direction == Direction.Left || direction == Direction.Right) walk_offset += 4;
-		sr.flipX = direction == Direction.Left ? true : false;
-		if(Input.GetKey(KeyCode.UpArrow))
-		{
-			keyDown = true;
-			direction = Direction.Up;
-			if(!walking) StartCoroutine("Walk");
-			walking = true;
-			transform.position += new Vector3(0f, moveSpeed, 0f);
-		}
-		if(Input.GetKey(KeyCode.DownArrow))
-		{
-			keyDown = true;
-			direction = Direction.Down;
-			if(!walking) StartCoroutine("Walk");
-			walking = true;
-			transform.position += new Vector3(0f, -moveSpeed, 0f);
-		}
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			keyDown = true;
-			direction = Direction.Left;
-			if(!walking) StartCoroutine("Walk");
-			walking = true;
-			transform.position += new Vector3(-moveSpeed, 0f, 0f);
-		}
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			keyDown = true;
-			direction = Direction.Right;
-			if(!walking) StartCoroutine("Walk");
-			walking = true;
-			transform.position += new Vector3(moveSpeed, 0f, 0f);
-		}
-		if(!keyDown) Idle();
-		QuantizePosition();
-
-		if(Mathf.Abs(transform.position.x % 20) == 10f)
-		{
-			if(old_x < transform.position.x) StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Right));
-			else StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Left));
-		}
-		if(Mathf.Abs(transform.position.y % 16) == 9f)
-		{
-			if(old_y < transform.position.y) StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Up));
-			else StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Down));
-		}
-	}
-
-	void QuantizePosition()
-	{
-		float x = Mathf.Round(transform.position.x * 8) / 8;
-		float y = Mathf.Round(transform.position.y * 8) / 8;
-		transform.position = new Vector3(x,y,0f); 
+		if( Input.GetKey(KeyCode.UpArrow) ||
+			Input.GetKey(KeyCode.DownArrow) ||
+			Input.GetKey(KeyCode.LeftArrow) ||
+			Input.GetKey(KeyCode.RightArrow)
+			) { walk.Walk(); return; }
+		Idle();
 	}
 
 	public void Idle()
 	{
 		walking = false;
-		StopCoroutine("Walk");
+		walk.Stop();
+
 		if(direction == Direction.Up) sr.sprite = back;
 		if(direction == Direction.Down) sr.sprite = front;
 		if(direction == Direction.Left || direction == Direction.Right) sr.sprite = side;
-		//sr.flipX = direction == Direction.Left ? true : false;
-	}
-
-	IEnumerator Walk() //can't just use FlipX anymore because shield sprites aren't mirrored
-	{
-		while(true)
-		{
-			sr.sprite = walk[walk_offset];
-			yield return new WaitForSeconds(0.125f);
-			sr.sprite = walk[walk_offset]; //duplicates improve game feel
-			yield return new WaitForSeconds(0.125f);
-			sr.sprite = walk[walk_offset+1];
-			yield return new WaitForSeconds(0.125f);
-			sr.sprite = walk[walk_offset+1];
-		}
 	}
 
 	void Fall()
---
+++ Assets/Scripts/Player/Walking.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+	public float moveSpeed;
+	//public bool onDoorTrigger;
+
+	[Header("Sprites")]
+	public Sprite[] walk;
+	int walk_offset;
+
+	CameraMovement cam;
+	SpriteRenderer sr;
+	Player player;
+
+	void Start()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		cam = Camera.main.GetComponent<CameraMovement>();
+		player = GetComponent<Player>();
+	}
+
+	public void Walk()
+	{
+		float old_x = transform.position.x;
+		float old_y = transform.position.y;
+		walk_offset = 0;
+		if(player.direction == Player.Direction.Down) walk_offset += 2;
+		if(player.direction == Player.Direction.Left || player.direction == Player.Direction.Right) walk_offset += 4;
+		sr.flipX = player.direction == Player.Direction.Left ? true : false;
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			player.direction = Player.Direction.Up;
+			if(!player.walking) StartCoroutine("Animation");
+			player.walking = true;
+			transform.position += new Vector3(0f, moveSpeed, 0f);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			player.direction = Player.Direction.Down;
+			if(!player.walking) StartCoroutine("Animation");
+			player.walking = true;
+			transform.position += new Vector3(0f, -moveSpeed, 0f);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			player.direction = Player.Direction.Left;
+			if(!player.walking) StartCoroutine("Animation");
+			player.walking = true;
+			transform.position += new Vector3(-moveSpeed, 0f, 0f);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			player.direction = Player.Direction.Right;
+			if(!player.walking) StartCoroutine("Animation");
+			player.walking = true;
+			transform.position += new Vector3(moveSpeed, 0f, 0f);
+		}
+		QuantizePosition();
+
+		if(Mathf.Abs(transform.position.x % 20) == 10f)
+		{
+			if(old_x < transform.position.x) StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Right));
+			else StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Left));
+		}
+		if(Mathf.Abs(transform.position.y % 16) == 9f)
+		{
+			if(old_y < transform.position.y) StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Up));
+			else StartCoroutine(cam.MoveCamera(CameraMovement.Direction.Down));
+		}
+	}
+
+	public void Stop() {StopCoroutine("Animation");}
+
+	void QuantizePosition()
+	{
+		float x = Mathf.Round(transform.position.x * 8) / 8;
+		float y = Mathf.Round(transform.position.y * 8) / 8;
+		transform.position = new Vector3(x,y,0f); 
+	}
+
+	IEnumerator Animation() //can't just use FlipX anymore because shield sprites aren't mirrored
+	{
+		while(true)
+		{
+			sr.sprite = walk[walk_offset];
+			yield return new WaitForSeconds(0.125f);
+			sr.sprite = walk[walk_offset]; //duplicates improve game feel
+			yield return new WaitForSeconds(0.125f);
+			sr.sprite = walk[walk_offset+1];
+			yield return new WaitForSeconds(0.125f);
+			sr.sprite = walk[walk_offset+1];
+		}
+	}
+}
