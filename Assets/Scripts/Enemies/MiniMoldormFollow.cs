ba03bd85cd9174575a55996bf6dee1ef5f253a8d
Charlee <liucharlee@gmail.com>
2020-11-30 20:40:06 -0800

    MoldormFollows successfully destroy tails code

--- Assets/Scripts/Enemies/MiniMoldormFollow.cs
+++ Assets/Scripts/Enemies/MiniMoldormFollow.cs
@@ -7,7 +7,6 @@ public class MiniMoldormFollow : Enemy
     public Transform BallToFollow;
     public float LerpScale=5f;
 
-
     void Start()
     {
         //BallToFollow = transform.parent;
@@ -15,14 +14,19 @@ public class MiniMoldormFollow : Enemy
 
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, BallToFollow.position, LerpScale * Time.deltaTime);
-
-        GameObject EnemyMoldorm = GameObject.Find("Mini-Moldorm");
+        if(BallToFollow == null){
+            Destroy(this.gameObject);
+        }
+        /*GameObject EnemyMoldorm = GameObject.Find("Mini-Moldorm");
         MiniMoldorm MoldormScript = EnemyMoldorm.GetComponent<MiniMoldorm>();
         if(MoldormScript.Lives <= 0){
 
             Destroy(this.gameObject);
-        }
+        }*/
+        
+        transform.position = Vector3.Lerp(transform.position, BallToFollow.position, LerpScale * Time.deltaTime);
+
+    
     }
 
     public override void SwordHit() {Debug.LogError("SwordHit() not implemented for " + gameObject.name + "!");}
