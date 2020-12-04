64476c8cf80e5edcde5e173c92c343dda635b2b8
sam <skb502@nyu.edu>
2020-12-03 23:09:18 -0500

    refactored half of AttackAndMove
    through line 140

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,14 +9,13 @@ public class Player : MonoBehaviour
     PlayerState currentState;
 
     ////////// PLAYER STATES //////////
-    Idle idle = new Idle();
-    Walk walk = new Walk();
-    Hit hit = new Hit();
-    Shield shield = new Shield();
-    Jump jump = new Jump();
-    Fall falling = new Fall();
-
-    public float moveSpeed = 5f;
+    Idle idle;
+    Walk walk;
+    Hit hit;
+    Shield shield;
+    Jump jump;
+    Fall fall;
+    Push push;
 
     public enum Direction {
         Up,
@@ -26,44 +25,71 @@ public class Player : MonoBehaviour
     }
     public Direction currentDirection;
 
+    Transform thisTransform;
+    CameraMovement cam;
+
     void Start()
     {
+        thisTransform = this.GetComponent<Transform>();
+
+        idle = new Idle(thisTransform);
+        walk = new Walk(thisTransform);
+        hit = new Hit(thisTransform);
+        shield = new Shield(thisTransform);
+        jump = new Jump(thisTransform);
+        fall = new Fall(thisTransform);
+        push = new Push(thisTransform);
+
         currentState = idle;
     }
 
     void Update()
     {
         if (cam.Panning) return;
+        
 
-        if (Input.GetKeyDown(KeyCode.X)) {
-            currentState = hit;
-        } 
-        else if (Input.GetKeyDown(KeyCode.Z)) {
-            currentState = shield;
-        } 
-        else if (Input.GetKeyDown(KeyCode.Space)) {
-            currentState = jump;
-        }
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+        // these if statements are honestly still hell and there's probably a more elegant
+        // way of doing this ... 
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) { // UP
             currentState = walk;
             currentDirection = Direction.Up;
         }
         
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) { // LEFT
             currentState = walk;
             currentDirection = Direction.Left;
         }
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
-            currentState = walK;
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) { // DOWN
+            currentState = walk;
             currentDirection = Direction.Down;
         }
 
-        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {  
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) { // RIGHT
             currentState = walk;
             currentDirection = Direction.Right;
         }
+
+        if (Input.GetKeyDown(KeyCode.X)) { // ATTACK
+            currentState = hit;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Z)) { // SHIELD
+            currentState = shield;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Space)) { // JUMP
+            currentState = jump;
+        } 
+
+        // bug: the way it's currently written, this will set it to idle on every frame
+        else { // IDLE IF NO BUTTONS ARE PRESSED
+            currentState = idle;
+        }
+
+        ///////////// UPDATE CURRENT STATE VALUES //////////////
+        currentState.setDirection(currentDirection);
+
+        if (currentState == walk && currentState.CheckPush()) 
+            currentState = push;
 
         currentState.UpdateOnActive();
     }
