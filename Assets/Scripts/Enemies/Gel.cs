5d1f562889ca407f08c392f0066aa705182337b6
Charlee <liucharlee@gmail.com>
2020-12-08 14:07:14 -0800

    Upgraded Appliance of death animation to red gel enemy
    -death animation game object prefab
    -instantiation and scaling of animation code

--- Assets/Scripts/Enemies/Gel.cs
+++ Assets/Scripts/Enemies/Gel.cs
@@ -8,7 +8,9 @@ public class Gel : Enemy
 
     Animator myAnimator;
 
-    private float isDead;  
+    //private float isDead;  
+
+    public GameObject deathAnimation;
 
     void Start()
     {
@@ -16,24 +18,25 @@ public class Gel : Enemy
 
         myAnimator = GetComponent<Animator>();
 
-        isDead = 0;
+        //isDead = 0;
     }
 
     void FixedUpdate()
     {
-        if (isDead == 0){
+
             var playerVector = playerTransform.transform.position;
             Vector3 followVector = playerVector - transform.position;
             transform.position += followVector.normalized * Time.deltaTime;
 
             Debug.DrawLine( transform.position, playerVector, Color.yellow);
-        }
     }
 
     public override void SwordHit() {
-        isDead = 1;
-        myAnimator.SetBool("isDead", true);
-        Destroy(this.gameObject, 0.7f);
-        //Destroy(this.gameObject);
+        //isDead = 1;
+        var instantiatedPrefab = Instantiate (deathAnimation, transform.position, Quaternion.identity) as GameObject;
+        instantiatedPrefab.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        Destroy(this.gameObject);
+        //myAnimator.SetBool("isDead", true);
+        //Destroy(this.gameObject, 0.7f);
     }
 }
