00c904b95c862f5fe8d8fc5d4c0e6e72838d4e40
Alex Gould <ag4536@nyu.edu>
2020-12-14 19:11:27 -0500

    added switch

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	public bool enemiesCanTrigger;
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if(c.tag == "Enemy" && enemiesCanTrigger) ActivateEnemy();
+		if(c.tag == "Player") Activate();
+	}
+
+	public virtual void Activate()
+	{
+		Debug.LogError(gameObject.name + " has no Activate() function!");
+	}
+
+	public virtual void ActivateEnemy()
+	{
+		Debug.LogError(gameObject.name + " is marked as trigger-able by an enemy, but has no ActivateEnemy()!");
+	}
+}
