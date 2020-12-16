c42ed7da477e4d11ac082bbd33b21f318d1452c1
Alex Gould <ag4536@nyu.edu>
2020-12-16 18:18:38 -0500

    oops

--- Assets/Scripts/Player/Fall.cs
+++ Assets/Scripts/Player/Fall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class fall : MonoBehaviour
+public class Fall : MonoBehaviour
 {
     float ResetTimer = 0.8f;
     
