0c06f8524dc4ee3b506b3b058d4685ce4d52d447
Alex Gould <ag4536@nyu.edu>
2020-12-16 17:47:50 -0500

    set var back

--- Assets/Scripts/Player/Fall.cs
+++ Assets/Scripts/Player/Fall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class fall : MonoBehaviour
 {
-    float ResetTimer = 45f;
+    float ResetTimer = 3f;
     
     void Update()
     {   
