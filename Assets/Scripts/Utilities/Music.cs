1b06143adf3a92e4328b82055a50fe1852b9b9d7
Alex Gould <ag4536@nyu.edu>
2020-12-16 13:48:51 -0500

    aaaaaah

---
+++ Assets/Scripts/Utilities/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	public AudioClip start;
+	public AudioClip end;
+
+	AudioSource sound;
+
+	void Start()
+	{
+		sound = GetComponent<AudioSource>();
+		sound.clip = start;
+		sound.Play();
+	}
+
+	public void Switch()
+	{
+		sound.clip = end;
+	}
+}
